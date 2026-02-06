4d3e17276845a6c0a0384eb9eb21795cfe96c028
arl-creator <213111769@upmh.edu.mx>
2026-02-06 15:04:52 -0600

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import uuid
 from pydub import AudioSegment
 import tempfile
 import difflib
-
+import json
 
 
 # Cargar API Key
@@ -422,8 +422,9 @@ def oracion_vocal():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    #port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
